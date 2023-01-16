9b2060c4959055a8128dc417faa0876756f45999
zlfmaodtod77 <zlfmaodtod77@naver.com>
2023-01-16 17:16:28 +0900

    과제제출

--- app.js
+++ app.js
@@ -1,35 +1,66 @@
 const express = require('express');
-// const cookieParser = require('cookieparser');
-
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const port = 9091;
 
-// app.use(cookieParser());
+app.use(cookieParser());
+app.use(express.json());
 
-app.get('/query', (req, res) => {
-    console.log(req.query.name)
+app.get('/list', (req, res) => {
+    const { keyword } = req.query;
+    console.log(keyword);
+    res.send("게시글 리스트")
 });
 
-app.post('/login', (req, res) => {
-    const {email, paaword } = req.body
-    console.log ("email", email)
-    console.log ("password", password)
-    res.send("/login api")
+app.get('/users/:id',(req, res) => {
+    const { id } = req.params;
+    console.log(id);
+    res.send("회원정보 한 명")
 });
 
-app.get('/headers', (req, res) => {
-    console.log(req.headers)
+app.get('/posts/:postid',(req, res) => {
+    const { postid } = req.params;
+    console.log(postid);
+    res.send("게시글 상세")
 });
 
-app.get('/cookies', (req, res) => {
-    console.log(req.cookies)
+app.post('/login', (req, res) =>{
+   const {email,password} = req.body;
+   console.log("email", email);
+   console.log("password", password);
+   res.send("로그인")
 });
 
-app.get('/body', (req, res) => {
-    console.log(req.body)
+app.post('/signup', (req, res) => {
+    const { id, password , nikname } = req.body;
+    console.log("id", id);
+    console.log("password", password);
+    console.log("nikname", nikname);
+    res.send("회원가입")
 });
 
+app.post('/posts/write',(req, res) => {
+    const { title, content } = req.body;
+    console,log("title", title);
+    console.log("content", content);
+    res.send("게시글 작성")
+});
+
+app.post('/posts/:postid/edit',(req, res) => {
+    const { postid } = req.params;
+    const { title, content } = req.body;
+    console.log(postid);
+    console.log("title", title);
+    console.log("content", content);
+    res.send("게시글 수정")
+});
+
+app.post('/posts/:postid/delete',(req, res) => {
+    const { postid} = req.params;
+    console.log(postid) ;
+    res.send("게시글 삭제")
+});
 
 
 
--- for.js
+++ for.js
@@ -1,14 +1,16 @@
 for (let i = 10000; i >= 1; i--) {
     console.log(i)
-}                       //10000 ~ 1 거꾸로 
+}                       //10000 ~ 1 거꾸로    // i 는 10000 이고 i 가 1 이랑 같아 질떄 까지 i를 하나씩 -- 한다
 
 for (let i = 1; i < 10000; i++){
    if (i % 2 === 1)
-    console.log(i)    // 1 ~ 10000 홀수  
+    console.log(i)    // 1 ~ 10000 홀수   
 }
 
 for (let i = 1; i<= 9; i++){
     for (let j = 1; j <= 9; j++){
         console.log(j * i)
     }                           // 구구단
+
 }
+
