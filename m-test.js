e0937d64cbedbec6bf69ad3c1dd707c601cc5629
zlfmaodtod77 <zlfmaodtod77@naver.com>
2023-01-18 19:23:12 +0900

    movies

---
+++ m-test.js
@@ -0,0 +1,87 @@
+const scores = [
+    {name: '지영', score: 80, mbti: 'INFP', addr: "지구 어딘가 ", contact: "010-1234-1234"},
+    {name: '효원', score: 50, mbti: 'INFP', addr: "지구 어딘가 ", contact: "010-1234-1234"},
+    {name: '정기', score: 60, mbti: 'INFP', addr: "지구 어딘가 ", contact: "010-1234-1234"},
+    {name: '대신', score: 30, mbti: 'INFP', addr: "지구 어딘가 ", contact: "010-1234-1234"},
+    {name: '택환', score: 94, mbti: 'INFP', addr: "지구 어딘가 ", contact: "010-1234-1234"},
+    {name: '호승', score: 30, mbti: 'INFP', addr: "지구 어딘가 ", contact: "010-1234-1234"},
+    {name: '희선', score: 23, mbti: 'INFP', addr: "지구 어딘가 ", contact: "010-1234-1234"},
+    {name: '설인', score: 56, mbti: 'INFP', addr: "지구 어딘가 ", contact: "010-1234-1234"},
+]
+
+// 과제2
+// 배열에서 40점 미만 점수 받은 자 구하기 for 문 의 경우에는 전부 이름으로 된 배열을 반환하는 한편,
+//  filter 를 사용하면 배열내 객체들로 이루어진 배열을 반환한다. for 문을 사용 했을때 처럼 이름으로만 이루어진 배열로 반환하도록 수정한다.
+
+const 빠따 = []
+for(let i = 0; i < scores.length; i++) {            //i 는 0이고 i 의 scores 를 한번씩 돈다 
+    if(scores[i].score < 40) {                      //이때 scores 의 score 가 40 보다 낮은 
+        빠따.push(scores[i].name)                   // 이름을 빠따에 넣는다 
+    }
+};
+
+console.log(빠따);          
+
+const 빠따 = []
+for(let i =0; i < score.length; i++) {
+    if(scores[i].score < 40) {
+        빠따.push(score[i].name)
+    }
+};
+console.log(빠따);
+
+const 빠따 = []
+for(let i = 0; i < Score.length i++) {
+    if(scores[i].score < 40) {
+        빠따.push(score[i].name)
+    }
+};
+console.log(빠따);
+
+const 빠따 = []
+for(let i = 0; i < score.length i++) {
+    if(score[i].score < 40) {
+        빠따.push(score[i].name)
+    }
+};
+console.log(빠띠);
+
+const 빠따 = []
+for(let i = 0; i < score.length i++) {
+    if(score[i].score < 40) {
+        빠따.push(score[i].name)
+    }
+};
+console.log(빠따);
+
+const 빠따 = []
+for(let i = 0; i < score.length i++) {
+    if(score[i].score < 40) {
+        빠따.push(score[i].name)
+    }
+};
+console.log(빠따);
+
+const 빠따 = [] 
+for(let i = 0; i < score.length i ++) {
+    if(score[i].score < 40) {
+        빠따.push(score[i].name)
+    }
+};
+console.log(빠따);
+
+const 빠따 = []
+for(let i = 0; i < score.length i++) {
+    if(score[i].score < 40) {
+        빠따.push(score[i].name)
+    }
+};
+console.log(빠따);
+
+const 빠따 = []
+for(let i = 0; i < score.length i++) {
+    if(score[i].score < 40) {
+        빠따.push(score[i].name)
+    }
+};
+console.log(빠따);
